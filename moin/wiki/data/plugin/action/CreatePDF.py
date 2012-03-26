202855f82410b89e2e8132e7f6612548055416f7
Holger Karl <hkarl@ieee.org>
2012-03-26 15:54:07 +0200

    fixed bug in exception handling, perhaps

--- moin/wiki/data/plugin/action/CreatePDF.py
+++ moin/wiki/data/plugin/action/CreatePDF.py
@@ -19,8 +19,8 @@ def execute (pagename, request):
     try: 
         out = subprocess.check_output (['make', 'moinpdf'], cwd=makepath, stderr=subprocess.STDOUT)
         request.theme.add_msg ("The pdf has been created successfully.")        
-    except subprocess.CalledProcessError as (returncode, output):
-        request.theme.add_msg ("The pdf has been created with return code %d, this usually signifies an error.\n Output: %s" % (returncode, output))
+    except subprocess.CalledProcessError, e:
+        request.theme.add_msg ("The pdf has been created with return code %d, this usually signifies an error.\n Output: %s" % (e.returncode, e.output))
     except OSError as (errno, strerror):
         request.theme.add_msg ("Operating system raised an error with code %d and description:  %s." % (errno, strerror))
     except:
